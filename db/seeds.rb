9b71d3b95c906a6c59b6f867afa8fbcdf6082e7c
Alexander Cheung <cheung.ying.lon@gmail.com>
2017-10-05 23:08:36 -0500

    [MER-194] default admin accounts in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,3 +41,34 @@ if !Business.all.any?
 		end
 	end
 end
+
+# create admin user accounts
+users = [
+	{
+		email: "cheung.ying.lon@gmail.com",
+		name: "alex cheung",
+	},
+	{
+		email: "jordanwan210@gmail.com",
+		name: "jordan wan",
+	},
+	{
+		email: "melodytay1992@gmail.com",
+		name: "mello tay",
+	},
+]
+users.each do |params|
+	if !User.where(email: params[:email]).any?
+		user = User.new(
+			email: params[:email],
+			name: params[:name],
+			password: "password",
+			role: "admin",
+		)
+		if user.save
+			puts "created user: #{user.email}"
+		else
+			puts "error creating user #{user.email}: " + user.errors.full_messages.first
+		end
+	end
+end
