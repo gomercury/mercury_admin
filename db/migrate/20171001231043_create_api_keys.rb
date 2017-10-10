ec6055465bdc64dcdecc7e0a22f0a42c9b4f0258
Alexander Cheung <cheung.ying.lon@gmail.com>
2017-10-10 15:45:57 -0500

    add mercury business id to businesses index table

--- db/migrate/20171001231043_create_api_keys.rb
+++ db/migrate/20171001231043_create_api_keys.rb
@@ -2,7 +2,7 @@ class CreateApiKeys < ActiveRecord::Migration[5.0]
   def change
     create_table :api_keys do |t|
       t.string :access_token, null: false
-      t.string :business_id, null: false
+      t.integer :business_id, null: false
       t.timestamps
     end
 
